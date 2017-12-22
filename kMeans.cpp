2729331edb580bf5894842e74e3de86c99073b2c
Joe Meyer <josephedwardmeyer@gmail.com>
2017-12-22 13:21:29 -0600

    increment and formatting

--- kMeans.cpp
+++ kMeans.cpp
@@ -26,14 +26,14 @@ void KMeans::Run()
     int _membershipChange = 10001;
     int epochNum = 0;
     while (_membershipChange > 10000)
-    {      
-      epochNum++;
-      std::cout << "Epoch " << epochNum << ": changing memberships" << std::endl;
-      
-      _membershipChange = ChangeMemberships();
-      RecalculateCentroids();
+    {
+        epochNum++;
+        std::cout << "Epoch " << epochNum << ": changing memberships" << std::endl;
+
+        _membershipChange = ChangeMemberships();
+        RecalculateCentroids();
     }
-    std::cout<< "Completed clustering with " << _membershipChange << " changed frame-label pairs." << std::endl;
+    std::cout << "Completed clustering with " << _membershipChange << " changed frame-label pairs." << std::endl;
 }
 
 // Initializes the clusers to guaranteed random points
@@ -71,7 +71,7 @@ int KMeans::ChangeMemberships()
     int _membershipChange = 0;
 
     std::map<int, int> my_map;
-      
+
     // for each DataPoint (DataPoints[i])
     for (int i = 0; i < DataPoints.size(); i++)
     {
@@ -94,81 +94,77 @@ int KMeans::ChangeMemberships()
 
         // For each cluster
         for (int j = 0; j < NumClusters; j++)
-          {
+        {
             // Calculate distance between DataPoint[i] and Clusters[j]
             double _potentialNewDistance = DataPoints[i].CalculateDistance(Centroids[j]);
             if (_potentialNewDistance < _currentDistance)
 
-              {
+            {
                 // if DataPoint[i] is closer to Clusters[j] than it was to its
                 // past cluster, reassign it
                 DataPoints[i].CentroidIndex = j;
                 _currentDistance = _potentialNewDistance;
                 _membershipChange++;
-                
+
                 // debugging
                 //std::cout << DataPoints[i].CentroidIndex << std::endl;
-              }
-          }
+            }
+        }
 
         // if the map doesnt have the key, set to 1
-        if ( my_map.find(DataPoints[i].CentroidIndex) == my_map.end() ) {
-          my_map[DataPoints[i].CentroidIndex] = 1;
-        } else {
-          // if the map does have the key, increment
-          my_map[DataPoints[i].CentroidIndex] = my_map[DataPoints[i].CentroidIndex]+1;
+        if (my_map.find(DataPoints[i].CentroidIndex) == my_map.end())
+        {
+            my_map[DataPoints[i].CentroidIndex] = 1;
         }
-         
+        else
+        {
+            // if the map does have the key, increment
+            my_map[DataPoints[i].CentroidIndex] = my_map[DataPoints[i].CentroidIndex] + 1;
+        }
     }
 
-    // print out all the <centroidID, numFrames> items 
-    for(auto item : my_map)
-      {
+    // print out all the <centroidID, numFrames> items
+    for (auto item : my_map)
+    {
         std::cout << item.first << " " << item.second << "\n";
-      }
+    }
     return _membershipChange;
 }
-
 
 // Makes the clusters homogenized
 bool KMeans::HomogenizeClusters()
 {
     bool _membershipChange = false;
-    
+
     // declare orgLabel: newCluster: numFramesInCluster
     std::map<int, std::map<int, int>> _labelToAllCentroids;
-    // fill in map 
-    for (int i = 0; i < DataPoints.size(); i++) {
-      // if the map doesnt have the nested keys, set to 1
-      if (
-          _labelToAllCentroids.find(DataPoints[i].Label)
-          == _labelToAllCentroids.end()
-          &&
-          _labelToAllCentroids[DataPoints[i].Label].find(DataPoints[i].CentroidIndex)
-          == _labelToAllCentroids[DataPoints[i].Label].end()
-          )
+    // fill in map
+    for (int i = 0; i < DataPoints.size(); i++)
+    {
+        // if the map doesnt have the nested keys, set to 1
+        if (
+            _labelToAllCentroids.find(DataPoints[i].Label) == _labelToAllCentroids.end() &&
+            _labelToAllCentroids[DataPoints[i].Label].find(DataPoints[i].CentroidIndex) == _labelToAllCentroids[DataPoints[i].Label].end())
         {
-          _labelToAllCentroids[DataPoints[i].Label][DataPoints[i].CentroidIndex] = 1;
+            _labelToAllCentroids[DataPoints[i].Label][DataPoints[i].CentroidIndex] = 1;
         }
-      else
+        else
         {
-          // if the map does have the key, increment
-          _labelToAllCentroids[DataPoints[i].Label][DataPoints[i].CentroidIndex]
-            = _labelToAllCentroids[DataPoints[i].Label][DataPoints[i].CentroidIndex]+1;
+            // if the map does have the key, increment
+            _labelToAllCentroids[DataPoints[i].Label][DataPoints[i].CentroidIndex]++;
         }
     }
-    
+
     std::map<int, int> _labelToCentroid;
     for (int i = 0; i < MaxLabels; i++)
-      {
+    {
         if (_labelToAllCentroids[i].size() < 1)
-          {
+        {
             // loop over all clusters, pick highest
-          }
-      }
+        }
+    }
     return _membershipChange;
 }
-
 
 // Uses new data to recalcultate centroid points
 void KMeans::RecalculateCentroids()
@@ -177,8 +173,8 @@ void KMeans::RecalculateCentroids()
     for (int i = 0; i < NumClusters; i++)
     {
         int _total = 0;
-        std::array<double, 13> _clusterSum = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-                                              0.0,0.0,0.0,0.0,0.0,0.0};
+        std::array<double, 13> _clusterSum = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                              0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
         // For each dataPoint
         for (int j = 0; j < DataPoints.size(); j++)
         {
